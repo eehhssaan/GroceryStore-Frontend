59326a77cd842e9e5af6ea6a168a13fff584a587
Ehsan <ehsan.ayaz@preface.education>
2022-06-12 16:58:16 +0800

    adding develop branch

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import Link from 'next/link';
-import Head from 'next/head';
-import Image from 'next/image';
+import React from "react";
+import Link from "next/link";
+import Head from "next/head";
+import Image from "next/image";
 
 const NotFound = () => {
   return (
     <>
       <Head>
-        <title>KachaBazar | 404</title>
+        <title>Kacha Bazar | 404</title>
         <meta name="description" content="This is page not found 404 page" />
         <link rel="icon" href="/favicon.png" />
       </Head>
