33b5ccbf5438cb4b7b943cf79bce55177c5bae8f
Ehsan <ehsan.ayaz@preface.education>
2022-07-07 17:47:41 +0800

    clean up of footer and social media links remove

--- src/layout/footer/Footer.js
+++ src/layout/footer/Footer.js
@@ -21,6 +21,98 @@ const Footer = () => {
   return (
     <div className="pb-16 lg:pb-0 xl:pb-0 bg-white">
       <div className="mx-auto max-w-screen-2xl px-4 sm:px-10">
+        <div className="mx-auto max-w-screen-2xl px-4 sm:px-10 bg-gray-50 shadow-sm border border-gray-50 rounded-lg">
+          <div className=" flex grid grid-cols-1 md:grid-cols-1 xl:grid-cols-1 gap-5 sm:gap-9 lg:gap-11 xl:gap-7 py-8 items-center justify-center">
+            {/* <div className="col-span-1">
+              <span className="text-base leading-7 font-medium block mb-2 pb-0.5">
+                Follow Us
+              </span>
+              <ul className="text-sm flex">
+                <li className="flex items-center mr-3 transition ease-in-out duration-500">
+                  <Link href="https://www.facebook.com">
+                    <a
+                      aria-label="Social Link"
+                      rel="noreferrer"
+                      target="_blank"
+                      className="block text-center mx-auto text-gray-500 hover:text-white"
+                    >
+                      <FacebookIcon size={34} round />
+                    </a>
+                  </Link>
+                </li>
+                <li className="flex items-center  mr-3 transition ease-in-out duration-500">
+                  <Link href="https://twitter.com">
+                    <a
+                      aria-label="Social Link"
+                      rel="noreferrer"
+                      target="_blank"
+                      className="block text-center mx-auto text-gray-500 hover:text-white"
+                    >
+                      <TwitterIcon size={34} round />
+                    </a>
+                  </Link>
+                </li>
+                <li className="flex items-center mr-3 transition ease-in-out duration-500">
+                  <Link href="https://www.pinterest.com">
+                    <a
+                      aria-label="Social Link"
+                      rel="noreferrer"
+                      target="_blank"
+                      className="block text-center mx-auto text-gray-500 hover:text-white"
+                    >
+                      <PinterestIcon size={34} round />
+                    </a>
+                  </Link>
+                </li>
+                <li className="flex items-center  mr-3 transition ease-in-out duration-500">
+                  <Link href="https://www.linkedin.com">
+                    <a
+                      aria-label="Social Link"
+                      rel="noreferrer"
+                      target="_blank"
+                      className="block text-center mx-auto text-gray-500 hover:text-white"
+                    >
+                      <LinkedinIcon size={34} round />
+                    </a>
+                  </Link>
+                </li>
+                <li className="flex items-center  mr-3 transition ease-in-out duration-500">
+                  <Link href="https://www.whatsapp.com">
+                    <a
+                      aria-label="Social Link"
+                      rel="noreferrer"
+                      target="_blank"
+                      className="block text-center mx-auto text-gray-500 hover:text-white"
+                    >
+                      <WhatsappIcon size={34} round />
+                    </a>
+                  </Link>
+                </li>
+              </ul>
+            </div> */}
+            <div className="flex col-span-1 justify-center">
+              <p className="text-base leading-7 font-medium block m-3">
+                Call Us Today!
+              </p>
+              <h5 className="text-2xl font-bold text-emerald-500 leading-7 m-3">
+                +012345-67900
+              </h5>
+            </div>
+            {/* <div className="col-span-1 hidden lg:block md:block">
+              <ul className="lg:text-right">
+                <li className="px-1 mb-2 md:mb-0 transition hover:opacity-80 inline-flex">
+                  <Image
+                    width={274}
+                    height={85}
+                    className="w-full"
+                    src="/payment-method/payment-logo.png"
+                    alt="payment method"
+                  />
+                </li>
+              </ul>
+            </div> */}
+          </div>
+        </div>
         <div className="grid grid-cols-2 md:grid-cols-7 xl:grid-cols-12 gap-5 sm:gap-9 lg:gap-11 xl:gap-7 py-10 lg:py-16 justify-between">
           <div className="pb-3.5 sm:pb-0 col-span-1 md:col-span-2 lg:col-span-3">
             <h3 className="text-md lg:leading-7 font-medium mb-4 sm:mb-5 lg:mb-6 pb-0.5">
@@ -155,99 +247,6 @@ const Footer = () => {
         </div>
 
         <hr className="hr-line"></hr>
-
-        <div className="mx-auto max-w-screen-2xl px-4 sm:px-10 bg-gray-50 shadow-sm border border-gray-50 rounded-lg">
-          <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-3 gap-5 sm:gap-9 lg:gap-11 xl:gap-7 py-8 items-center justify-between">
-            <div className="col-span-1">
-              <span className="text-base leading-7 font-medium block mb-2 pb-0.5">
-                Follow Us
-              </span>
-              <ul className="text-sm flex">
-                <li className="flex items-center mr-3 transition ease-in-out duration-500">
-                  <Link href="https://www.facebook.com">
-                    <a
-                      aria-label="Social Link"
-                      rel="noreferrer"
-                      target="_blank"
-                      className="block text-center mx-auto text-gray-500 hover:text-white"
-                    >
-                      <FacebookIcon size={34} round />
-                    </a>
-                  </Link>
-                </li>
-                <li className="flex items-center  mr-3 transition ease-in-out duration-500">
-                  <Link href="https://twitter.com">
-                    <a
-                      aria-label="Social Link"
-                      rel="noreferrer"
-                      target="_blank"
-                      className="block text-center mx-auto text-gray-500 hover:text-white"
-                    >
-                      <TwitterIcon size={34} round />
-                    </a>
-                  </Link>
-                </li>
-                <li className="flex items-center mr-3 transition ease-in-out duration-500">
-                  <Link href="https://www.pinterest.com">
-                    <a
-                      aria-label="Social Link"
-                      rel="noreferrer"
-                      target="_blank"
-                      className="block text-center mx-auto text-gray-500 hover:text-white"
-                    >
-                      <PinterestIcon size={34} round />
-                    </a>
-                  </Link>
-                </li>
-                <li className="flex items-center  mr-3 transition ease-in-out duration-500">
-                  <Link href="https://www.linkedin.com">
-                    <a
-                      aria-label="Social Link"
-                      rel="noreferrer"
-                      target="_blank"
-                      className="block text-center mx-auto text-gray-500 hover:text-white"
-                    >
-                      <LinkedinIcon size={34} round />
-                    </a>
-                  </Link>
-                </li>
-                <li className="flex items-center  mr-3 transition ease-in-out duration-500">
-                  <Link href="https://www.whatsapp.com">
-                    <a
-                      aria-label="Social Link"
-                      rel="noreferrer"
-                      target="_blank"
-                      className="block text-center mx-auto text-gray-500 hover:text-white"
-                    >
-                      <WhatsappIcon size={34} round />
-                    </a>
-                  </Link>
-                </li>
-              </ul>
-            </div>
-            <div className="col-span-1 text-center hidden lg:block md:block">
-              <p className="text-base leading-7 font-medium block">
-                Call Us Today!
-              </p>
-              <h5 className="text-2xl font-bold text-emerald-500 leading-7">
-                +012345-67900
-              </h5>
-            </div>
-            <div className="col-span-1 hidden lg:block md:block">
-              <ul className="lg:text-right">
-                <li className="px-1 mb-2 md:mb-0 transition hover:opacity-80 inline-flex">
-                  <Image
-                    width={274}
-                    height={85}
-                    className="w-full"
-                    src="/payment-method/payment-logo.png"
-                    alt="payment method"
-                  />
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
       </div>
 
       <div className="mx-auto max-w-screen-2xl px-3 sm:px-10 flex justify-center py-4">
